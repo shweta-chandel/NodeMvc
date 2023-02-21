9cbb3b5a7d87f77c35d2c44aea47b38dbdb1036a
shweta chandel <shwetachandel89@gmail.com>
2023-02-21 23:17:01 +0530

    add

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const mysql = require('mysql');
 const express = require('express');
+const path =require("path");
+const bodyparser = require('body-parser');
 const app = express();
+app.use(bodyparser.json());
+var validator = require("email-validator");
+const bcrypt = require('bcrypt');
 
 
 
@@ -18,7 +23,7 @@ conn.connect((err) =>{
 
 
 app.get('/',(req, res) => {
-  let sql = "SELECT * FROM registration";
+  let sql = "SELECT * FROM register";
   let query = conn.query(sql, (err, results) => {
     if(err) throw err;
     res.send(results)
@@ -26,37 +31,86 @@ app.get('/',(req, res) => {
 });
 
 
-app.post('/registration', (req, res) => {
-  console.log(req.body)
-  const email_id = req.body.email_id;
-  const mobile_no = req.body.mobile_no;
-  const password = req.body.password;
-  const confirm = req.body.confirm;
-  let data = { email_id, mobile_no, password, confirm };
-  if (!email_id || !mobile_no || !password) {
-    console.log(email_id)
-  return res.status(400).send('Please provide username, email, and password');
-  }
-  // if (confirm.value === password.value) {
-  //   res.send('matched');
-  // } else {
-  //   res.send('Passwords do not match');
-  // }
-  //   conn.query("SELECT * FROM registration WHERE email_id='" + email_id + "' limit 1", (err, rows, fields) => {
-  //     if (rows.length > 0) {
-  //       return res.send("already exist");
-  //     }
-      conn.query("INSERT INTO registration SET?", data, (err, results) => {
-        if (err) {
-          return res.send("Failed");
-        }
-        else {
-          return res.send("success");
-        }
-      });
-   });
 
-   
+app.post('/register', (req, res) => {
+  console.log(req.body)
+  const email = req.body.email;
+  //const mobile = req.body.mobile;
+  var password = req.body.password;
+  const confirm = req.body.confirm;
+  let data;
+  if (!email|| !password) {
+  return res.send('Please provide email and password');
+  }
+  isEmail = validator.validate(email);
+  if(!isEmail){
+    return res.send('Please insert correct email')
+  }
+  if (confirm !== password) {
+    res.send('Password does not match');
+  }
+
+ bcrypt.hash(password, 10, function(err, hash) {
+    password = hash;
+    data = {email, password}
+    conn.query("INSERT INTO register SET?", data, (err, results) => {
+      if (err) {
+        return res.send("Failed");
+      }
+      else {
+        return res.send("success");
+      }
+    });
+  });
+})
+  
+
+app.post('/update',(req, res) => {
+  console.log(req.body);
+  const mobile = req.body.mobile
+  let sql = "UPDATE register SET mobile='"+req.body.mobile+"', WHERE email="+req.body.email;
+  let query = conn.query(sql, (err, results) => {
+  if(err) throw err;
+    res.send("successfully mobile updated");
+  });
+});
+
+app.post('/login', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const data = {email, password};
+  conn.query("SELECT * FROM register WHERE email = ? AND password = ?", [email, password], function (err, rows) {
+    if (rows.length > 0) {
+      return res.send("logged in")
+    }
+    else {
+      return res.send("not found");
+    }
+  })
+})
+
+app.post('/forgotpassword', (req, res) => {
+  const email = req.body.email;
+  var newpassword = req.body.newpassword;
+  conn.query(`SELECT * FROM register WHERE email = "${email}"`, (err, results) => {
+    if (!err) {
+      if (results.length != 0) {
+        conn.query('UPDATE register set password = ? where email =?', [newpassword, email], (err, results) => {
+          if (!err) {
+            return res.send("forget password updated")
+          }
+          else {
+            return res.send(err);
+          }
+        })
+      } else {
+        return res.send("user not found cant updated forget password");
+      }
+    } else {
+      return res.send(err);
+    }
+  })
+})
 
 
 
