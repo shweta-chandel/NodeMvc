6909495e2fb28150929efd55c114268299925bb7
shweta chandel <shwetachandel89@gmail.com>
2023-02-21 15:35:59 +0530

    add

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ conn.connect((err) =>{
 
 
 app.get('/',(req, res) => {
-  let sql = "SELECT * FROM users";
+  let sql = "SELECT * FROM registration";
   let query = conn.query(sql, (err, results) => {
     if(err) throw err;
     res.send(results)
@@ -26,16 +26,27 @@ app.get('/',(req, res) => {
 });
 
 
-app.post('/register', (req, res) => {
-  const { username, email, password } = req.body;
-  if (!username || !email || !password) {
-    return res.status(400).send('Please provide username, email, and password');
+app.post('/registration', (req, res) => {
+  console.log(req.body)
+  const email_id = req.body.email_id;
+  const mobile_no = req.body.mobile_no;
+  const password = req.body.password;
+  const confirm = req.body.confirm;
+  let data = { email_id, mobile_no, password, confirm };
+  if (!email_id || !mobile_no || !password) {
+    console.log(email_id)
+  return res.status(400).send('Please provide username, email, and password');
   }
-    conn.query("SELECT * FROM users WHERE email='" + email + "' limit 1", (err, rows, fields) => {
-      if (rows.length > 0) {
-        return res.send("already exist");
-      }
-      conn.query("INSERT INTO users SET?", data, (err, results) => {
+  // if (confirm.value === password.value) {
+  //   res.send('matched');
+  // } else {
+  //   res.send('Passwords do not match');
+  // }
+  //   conn.query("SELECT * FROM registration WHERE email_id='" + email_id + "' limit 1", (err, rows, fields) => {
+  //     if (rows.length > 0) {
+  //       return res.send("already exist");
+  //     }
+      conn.query("INSERT INTO registration SET?", data, (err, results) => {
         if (err) {
           return res.send("Failed");
         }
@@ -43,9 +54,9 @@ app.post('/register', (req, res) => {
           return res.send("success");
         }
       });
-    });
-  })
-  
+   });
+
+   
 
 
 
