8758ae95ac89cb13de955ebc5bc3aa2515fa5109
shweta chandel <shwetachandel89@gmail.com>
2023-02-27 11:35:45 +0530

    add

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,7 @@
-var express = require("express");
-const AuthController = require("../../controllers/api/AuthController");
-
-var router = express.Router();
-
-router.post("/mobile", AuthController.mobile);
+const router = require('express').Router();
+const UserController = require('../../Controller/UserController');
+console.log(UserController)
+router.post("/mobile", UserController.mobile);
 router.post("/verify-otp", AuthController.verifyOtp);
 router.post("/complete-profile", AuthController.completeprofile);
 router.post("/login", AuthController.login);
