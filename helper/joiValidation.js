1088c84d0d4ec5272c3665f82a391ee3d89d730d
shweta chandel <shwetachandel89@gmail.com>
2023-02-28 16:27:53 +0530

    add

--- helper/joiValidation.js
+++
@@ -1,9 +0,0 @@
-module.exports.joiValidation  = (schema, data)=> {
-    const result = schema.validate(data);
-    if(result.error){
-        let error = {
-            message : result.error.details[0].message
-        }
-        throw error;
-    }
-}
